297b621cecf5b42fada26b19f10c8496020c01d6
Lohita Alekhya <alekhyakollu@gmail.com>
2026-04-17 11:55:56 +0530

    Enhanced the million user.sql

--- seeds/03-million-users.sql
+++ seeds/03-million-users.sql
@@ -2,8 +2,11 @@ DO $$
 DECLARE
   start_id INT := 1001;
   batch_size INT := 50000;
+  end_id INT;
 BEGIN
   WHILE start_id <= 1000000 LOOP
+    end_id := LEAST(start_id + batch_size - 1, 1000000);
+
     INSERT INTO users (name, email, country_code, subscription_tier, lifetime_value)
     SELECT
       'User ' || g,
@@ -11,7 +14,7 @@ BEGIN
       CASE WHEN g % 2 = 0 THEN 'IN' ELSE 'US' END,
       CASE WHEN g % 3 = 0 THEN 'premium' ELSE 'basic' END,
       (random() * 10000)::int
-    FROM generate_series(start_id, start_id + batch_size - 1) AS g;
+    FROM generate_series(start_id, end_id) AS g;
 
     start_id := start_id + batch_size;
   END LOOP;
